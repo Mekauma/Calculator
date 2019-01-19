1edfbb841302a5c3fa710d8cb22eb798fcbffa54
Mekauma <46765086+Mekauma@users.noreply.github.com>
2019-01-19 16:57:33 -0500

    adding division

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,4 +12,8 @@ public class Calculator {
     public static double mutiply(double num1, double num2){
         return num1 * num2;
     }
+    
+    public static double division(double num1, double num2){
+        return num1 /num2;
+    }
 }
